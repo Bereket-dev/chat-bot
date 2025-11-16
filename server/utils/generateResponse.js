4913761c72cc7b001756e01fa7591984f4012997
Bereket <bereketdesalegn097@gmail.com>
2025-11-17 01:05:17 +0300

    feat: enahnce result and case to prevent token limit display

--- server/utils/generateResponse.js
+++ server/utils/generateResponse.js
@@ -4,14 +4,29 @@ dotenv.config();
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-const generateResponse = async (message) => {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: message,
-  });
+const generateResponse = async (messages) => {
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: messages,
+    });
 
-  const responseString = response.text || "";
-  return responseString;
+    const responseString = response.text || "";
+    return responseString;
+  } catch (err) {
+    if (err.status === 429) {
+      // Extract retry delay from API error
+      const waitSeconds =
+        err.error?.details?.[2]?.retryDelay?.replace("s", "") || 20;
+      console.log(`⏳ Rate limited. Waiting ${waitSeconds}s before retry...`);
+
+      await new Promise((res) => setTimeout(res, waitSeconds * 1000));
+
+      return await generateResponse(messages); // retry again
+    }
+
+    throw err;
+  }
 };
 
 module.exports = generateResponse;
